c8271624c11f233da94ecc634d8882120912862d
Eduardo-Alves1 <eduardo.santos.alves14@gmail.com>
2024-11-12 17:44:11 -0300

    Brand end

--- brands/views.py
+++ brands/views.py
@@ -1,4 +1,4 @@
-from django.views.generic import ListView, CreateView, DetailView, UpdateView
+from django.views.generic import ListView, CreateView, DetailView, UpdateView, DeleteView
 from django.urls import reverse_lazy
 from . import models, forms
 
@@ -7,6 +7,7 @@ class BrandListView(ListView):
     model = models.Brand
     template_name = 'brand_list.html'
     context_object_name = 'brands'
+    paginate_by = 10
 
     def get_queryset(self): #Sobreescrevendo e Filtrando uma Brand pelo nome
         queryset = super().get_queryset()
@@ -32,4 +33,9 @@ class BrandUpdateView(UpdateView):
     model = models.Brand
     template_name = 'brand_update.html'
     form_class = forms.BrandForm
+    success_url = reverse_lazy('brand_list')
+
+class BrandDeleteView(DeleteView):
+    model = models.Brand
+    template_name = 'brand_delete.html'
     success_url = reverse_lazy('brand_list')
