c12aac4ecb6385d32b83cd9bb4242597756e681d
Eduardo Alves <eduardo.santos.alves14@gmail.com>
2024-11-27 20:04:27 -0300

    Create Inflows

--- inflows/views.py
+++ inflows/views.py
@@ -1,3 +1,41 @@
-from django.shortcuts import render
+from django.views.generic import ListView, CreateView, DetailView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+from . import models, forms
 
-# Create your views here.
+
+class InflowListView(ListView):
+    model = models.Inflow
+    template_name = 'inflow_list.html'
+    context_object_name = 'inflows'
+    paginate_by = 10
+
+    def get_queryset(self): #Sobreescrevendo e Filtrando uma Brand pelo nome
+        queryset = super().get_queryset()
+        name = self.request.GET.get('name')
+        print("VALOR NAME", name)
+
+        if name:
+            queryset = queryset.filter(name__icontains=name)
+        print("Queryset:", queryset)
+        return queryset
+
+class InflowCreateView(CreateView):
+    model = models.Inflow
+    template_name = 'inflow_create.html'
+    form_class = forms.InflowForm
+    success_url = reverse_lazy('inflow_list')
+
+class InflowDetailView(DetailView):
+    model = models.Inflow
+    template_name = 'inflow_detail.html'
+
+class InflowUpdateView(UpdateView):
+    model = models.Inflow
+    template_name = 'inflow_update.html'
+    form_class = forms.InflowForm
+    success_url = reverse_lazy('inflow_list')
+
+class InflowDeleteView(DeleteView):
+    model = models.Inflow
+    template_name = 'inflow_delete.html'
+    success_url = reverse_lazy('inflow_list')
